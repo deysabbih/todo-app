3b5d4fecf4f10779c03107c8d402c2c04f998e18
Hibba Syed <hibbasyed@Hibbas-MacBook-Air.local>
2025-08-26 22:40:39 -0400

    First commit - To-Do app tutorial project

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Invalid task");
+    }
+    else{
+        let li = document.createElement("li"); //add task
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span"); //x icon at the end of task
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},  false);
+
+// save tasks when reloading
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
